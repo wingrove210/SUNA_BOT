3dd0628d97daa2981a35b908c654a7b703f6da49
pashatrushin <trusinpavel34@gmail.com>
2025-06-01 14:41:04 +0300

    modified:   index.html 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,48 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoInsides from './pages/NoInsides'
-// import Chat from './pages/Chat'
+import { useEffect } from 'react'
 import FormFirst from './pages/Form/FormFirst'
 import FormSecond from './pages/Form/FormSecond'
 import './index.css'
 import Home from './pages/Home/Home'
+interface TelegramWebApp {
+  expand: () => void;
+  ready: () => void;
+  setHeaderColor: (color: string) => void;
+  requestFullscreen: () => void; // Добавьте эту строку
+  // Добавьте другие методы по необходимости
+}
 
-function App(){
-  return(
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: TelegramWebApp;
+      [key: string]: unknown;
+    };
+  }
+}
+
+const tg = window.Telegram?.WebApp;
+
+function App() {
+  useEffect(() => {
+    if (tg) {
+      tg.expand();
+      tg.requestFullscreen();
+      tg.ready(); 
+    }
+  }, []);
+  useEffect(() => {
+    if (tg) {
+      tg.ready();
+      tg.setHeaderColor("#474C2B");
+    }
+  }, []);
+
+  return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/noinsides" element={<NoInsides />} />
         {/* <Route path="/player" element={<Home />} /> */}
         <Route path="/form/first" element={<FormFirst />} />
@@ -19,4 +51,5 @@ function App(){
     </BrowserRouter>
   );
 }
+
 export default App;
