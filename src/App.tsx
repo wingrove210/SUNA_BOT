b401f5dc649ada9bde87cbfbe2bf786d1f9aa4fe
pashatrushin <trusinpavel34@gmail.com>
2025-06-03 02:36:58 +0300

    modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,62 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import NoInsides from './pages/NoInsides'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import FormFirst from './pages/Form/FormFirst'
 import FormSecond from './pages/Form/FormSecond'
 import Chat from './pages/Chat'
 import './index.css'
 import Home from './pages/Home/Home'
-import type { TelegramWebApp } from './types/telegram' // Импортируйте тип
+import type { TelegramWebApp } from './types/telegram'
 
 const tg = window.Telegram?.WebApp as TelegramWebApp | undefined;
 
+function isMobile() {
+  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
 function App() {
+  const [mobile, setMobile] = useState(true);
+
   useEffect(() => {
+    setMobile(isMobile());
     if (tg) {
       tg.expand();
       tg.requestFullscreen?.(); 
       tg.ready(); 
     }
   }, []);
+
   useEffect(() => {
     if (tg) {
       tg.ready();
     }
     if (tg && tg.initData) {
-    // Проверяем, открыто ли в мобильном Telegram
-    if (tg.isExpanded) {
-        console.log("Работает в мобильном Telegram");
-    } else {
+      if (tg.isExpanded) {
+        // ok
+      } else {
         document.body.innerHTML = "Откройте приложение в мобильном Telegram!";
+      }
+    } else {
+      document.body.innerHTML = "Этот сервис работает только внутри Telegram!";
     }
-} else {
-    document.body.innerHTML = "Этот сервис работает только внутри Telegram!";
-}
   }, []);
+
+  if (!mobile) {
+    return (
+      <div style={{
+        minHeight: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        background: "#111",
+        color: "#fff",
+        fontSize: 22,
+        textAlign: "center"
+      }}>
+        Откройте это приложение на мобильном устройстве
+      </div>
+    );
+  }
 
   return (
     <BrowserRouter>
