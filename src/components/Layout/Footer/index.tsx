33877733ca608a0e2bf3078ae1dc08b07ed46272
# <trusinpavel34@gmail.com>
2025-06-10 23:30:20 +0300

    modified:   src/components/Layout/Footer/index.tsx

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -7,31 +7,40 @@ const menuItemsData = [
 		color: "#ffb457",
 		path: "/",
 		icon: (
-			<svg
-				className="h-6 w-6"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-			>
-				<path d="M3.8,6.6h16.4" />
-				<path d="M20.2,12.1H3.8" />
-				<path d="M3.8,17.5h16.4" />
-			</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-7 w-7"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
+      ></path>
+    </svg>
 		),
 	},
 	{
 		color: "#ff96bd",
 		path: "/",
 		icon: (
-			<svg
-				className="h-6 w-6"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-			>
-				<path d="M6.7,4.8h10.7c0.3,0,0.6,0.2,0.7,0.5l2.8,7.3c0,0.1,0,0.2,0,0.3v5.6c0,0.4-0.4,0.8-0.8,0.8H3.8C3.4,19.3,3,19,3,18.5v-5.6c0-0.1,0-0.2,0.1-0.3L6,5.3C6.1,5,6.4,4.8,6.7,4.8z" />
-				<path d="M3.4,12.9H8l1.6,2.8h4.9l1.5-2.8h4.6" />
-			</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-7 w-7"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M7 11.5V14m0-2.5v-6a1.5 1.5 0 113 0m-3 6a1.5 1.5 0 00-3 0v2a7.5 7.5 0 0015 0v-5a1.5 1.5 0 00-3 0m-6-3V11m0-5.5v-1a1.5 1.5 0 013 0v1m0 0V11m0-5.5a1.5 1.5 0 013 0v3m0 0V11"
+      ></path>
+    </svg>
 		),
 	},
 	{
@@ -58,32 +67,46 @@ const menuItemsData = [
 		color: "#ffe797",
 		path: "/noinsides",
 		icon: (
-			<svg
-				className="h-6 w-6"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-			>
-				<path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1C3.9,4.4,4.4,3.9,5.1,3.9z" />
-				<path d="M4.2,9.3h15.6" />
-				<path d="M9.1,9.5v10.3" />
-			</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-7 w-7"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 10a1 1 0 011-1h4a1 1 0 011 1v4a1 1 0 01-1 1h-4a1 1 0 01-1-1v-4z"
+      ></path>
+    </svg>
 		),
 	},
 	{
 		color: "#cffff1",
 		path: "/chat",
 		icon: (
-			<svg
-				className="h-6 w-6"
-				viewBox="0 0 24 24"
-				fill="none"
-				stroke="currentColor"
-			>
-				<path d="M5.1,3.9h13.9c0.6,0,1.2,0.5,1.2,1.2v13.9c0,0.6-0.5,1.2-1.2,1.2H5.1c-0.6,0-1.2-0.5-1.2-1.2V5.1C3.9,4.4,4.4,3.9,5.1,3.9z" />
-				<path d="M5.5,20l9.9-9.9l4.7,4.7" />
-				<path d="M10.4,8.8c0,0.9-0.7,1.6-1.6,1.6c-0.9,0-1.6-0.7-1.6-1.6C7.3,8,8,7.3,8.9,7.3C9.7,7.3,10.4,8,10.4,8.8z" />
-			</svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-7 w-7"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 5v.01M12 12v.01M12 19v.01M12 6a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2zm0 7a1 1 0 110-2 1 1 0 010 2z"
+      ></path>
+    </svg>
 		),
 	},
 ];
@@ -99,7 +122,14 @@ export default function Footer() {
 
 	return (
 		<div className="w-full fixed bottom-4 z-50">
-			<div className="flex items-center justify-between bg-gray-950 bg-opacity-95 backdrop-blur-md rounded-4xl px-6 py-8 shadow-lg max-w-md mx-auto transition-all duration-300">
+			<div
+  className="flex items-center justify-between bg-[#25272A] bg-opacity-95 backdrop-blur-md rounded-4xl px-6 py-6 shadow-lg max-w-md mx-auto transition-all duration-300"
+  style={{
+    border: `2px solid ${menuItemsData[activeIdx].color}`,
+    boxShadow: `0 0 24px 0 ${menuItemsData[activeIdx].color}66`, // 66 = прозрачность
+    transition: "border 0.2s, box-shadow 0.2s",
+  }}
+>
 				{menuItemsData.map((item, idx) => (
 					<Link
 						to={item.path}
