dcbcced7dca9d5cbda4d317489aec9ca3dce8a2e
# <trusinpavel34@gmail.com>
2025-06-10 16:36:49 +0300

    modified:   src/pages/Chat/index.tsx

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -30,16 +30,29 @@ export default function Chat() {
   }, [navigate]);
 
   return (
-    <div className="text-white px-4 bg-black min-h-[100vh] pt-14">
-      <h1 className="text-2xl py-3 pl-4 font-light">Создать трек</h1>
-      <textarea
-        name="text"
-        id="text"
-        rows={5}
-        placeholder="Например: уютный зимний вечер"
-        className="text-sm w-full px-4 py-3 bg-[#232323] rounded-3xl border-[1px] border-gray-400"
-      ></textarea>
-      <div className="w-full text-sm flex flex-col gap-4 mt-1">
+    <div className="text-white bg-black min-h-[100vh] pt-14">
+      <h1
+        className="font-bold mb-4 uppercase text-center text-2xl tracking-[0.25em] text-white"
+        style={{
+          textShadow: "rgba(0, 0, 0) 0px 5px 45px, rgb(0, 0, 0) 0px 1px 9px",
+          fontFamily: "'Montserrat', 'Arial', sans-serif",
+          fontWeight: 700,
+          letterSpacing: "0.25em",
+          WebkitTextStroke: "2px #fff",
+          color: "transparent",
+        }}
+      >
+        Создать трек
+      </h1>
+      <div className="w-full h-auto bg-[#262727] pt-4 pb-2 rounded-xl px-4">
+        <textarea
+          name="text"
+          id="text"
+          rows={5}
+          placeholder="Например: уютный зимний вечер"
+          className="text-sm w-full px-4 py-3 bg-[#232323] rounded-3xl border-[1px] border-gray-400"
+        ></textarea>
+              <div className="w-full text-sm flex flex-col gap-4 mt-1">
         {["Придумай мне сценарий", "Мне нужен ХИТ"].map((label, idx) => (
           <button
             key={label}
@@ -56,8 +69,9 @@ export default function Chat() {
           </button>
         ))}
       </div>
-      <div className="w-full text-base gap-2 mt-2">
+       <div className="w-full text-base gap-2 mt-2">
         <FormFirst />
+      </div>
       </div>
     </div>
   );
